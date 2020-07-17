e93b45c7c0b9cf0dd56d3f79263193ebddbe20c7
Dahae Kim <iluvdadong@naver.com>
2020-07-17 16:38:35 +0900

    Setting up the Quizzler Project and Showing the Questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = ["베뉴 VS 셀토스" ,
+                "베뉴 VS 코나",
+                "베뉴 VS 티볼리"]
+    
+    var quizNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateQuizText()
+        //viewDidLoad는 앱 최초 실행 시 1번만 로딩됨
+        
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        quizNumber += 1
+        updateQuizText()
+        
+    }
+    
+    func updateQuizText() {
+        questionLabel.text = quiz[quizNumber]
+    }
 }
 
